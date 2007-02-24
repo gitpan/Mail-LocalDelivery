65eed7f614928756a77ac4aff0dd7f9520bb9bf1
Ricardo SIGNES <rjbs@cpan.org>
2007-02-24 15:08:55 +0000

    Import of RJBS/Mail-LocalDelivery-0.303 from CPAN.
    gitpan-cpan-distribution: Mail-LocalDelivery
    gitpan-cpan-version:      0.303
    gitpan-cpan-path:         RJBS/Mail-LocalDelivery-0.303.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Mail/LocalDelivery.pm
+++ lib/Mail/LocalDelivery.pm
@@ -17,7 +17,7 @@ use constant DEFERRED  => 75;
 use constant REJECTED  => 100;
 use constant DELIVERED => 0;
 
-$VERSION = '0.302';
+$VERSION = '0.303';
 
 =head1 NAME
 
@@ -25,9 +25,9 @@ Mail::LocalDelivery - Deliver mail to a local mailbox
 
 =head1 VERSION
 
-version 0.302
+version 0.303
 
- $Id: /my/pep/Mail-LocalDelivery/trunk/lib/Mail/LocalDelivery.pm 30759 2007-02-24T15:04:26.236527Z rjbs  $
+ $Id: /my/pep/Mail-LocalDelivery/trunk/lib/Mail/LocalDelivery.pm 30760 2007-02-24T15:06:21.283968Z rjbs  $
 
 =head1 SYNOPSIS
 
